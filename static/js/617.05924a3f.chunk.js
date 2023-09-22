7fbbc6eceeafbf1b90903d584cdbd8d56101e46a
irapanch <117355580+irapanch@users.noreply.github.com>
2023-09-22 18:52:21 +0000

    Deploying to gh-pages from @ irapanch/goit-react-hw-05-movies@b72fe42e8e3c98e9dec4f0e4c63a95b8a1a1e7aa 🚀

---
+++ static/js/617.05924a3f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[617],{8617:function(t,e,r){r.d(e,{Gjm:function(){return o}});var n=r(9983);function o(t){return(0,n.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",strokeWidth:"2",stroke:"currentColor","aria-hidden":"true"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",d:"M11 19l-7-7 7-7m8 14l-7-7 7-7"}}]})(t)}}}]);
+//# sourceMappingURL=617.05924a3f.chunk.js.map
